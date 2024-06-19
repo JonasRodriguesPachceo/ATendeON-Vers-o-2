726d14911276f1978db500f6b78c62a35523d0dc
Jenifer Osik <jenifer.osik@gmail.com>
2024-06-19 17:23:29 -0300

    Correção pastas vazias e mudança de nomenclatura

---
+++ back-end/src/config/redis.ts
@@ -0,0 +1,3 @@
+export const REDIS_URI_CONNECTION = "rediss://default:AVNS_K33JP3Ed78muu9Wo5zx@redis-ac6c9fa-fernandofitilan-c71c.e.aivencloud.com:27394";
+export const REDIS_OPT_LIMITER_MAX = process.env.REDIS_OPT_LIMITER_MAX || 1;
+export const REDIS_OPT_LIMITER_DURATION = process.env.REDIS_OPT_LIMITER_DURATION || 3000;
